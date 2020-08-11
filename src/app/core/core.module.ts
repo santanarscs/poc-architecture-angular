a25496dcde03b796966ccf7639e5aec35a87f71b
santanarscs <raphaelstn@gmail.com>
2020-08-11 18:38:36 -0300

    feat: update layout main layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
@@ -9,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
   ],
