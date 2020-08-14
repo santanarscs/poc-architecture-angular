63a657415445a27510b2eb2309fdcae5e6bc0ac2
santanarscs <raphaelstn@gmail.com>
2020-08-13 21:37:16 -0300

    refactor(shared): update types in crudservice
    update types in methods create and update

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDividerModule } from '@angular/material/divider';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { SharedModule } from '../../shared/shared.module';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatDividerModule } from '@angular/material/divider'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { SharedModule } from '../../shared/shared.module'
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
-import { DashboardService } from './services/dashboard.service';
+import { DashboardRoutingModule } from './dashboard-routing.module'
+import { DashboardComponent } from './dashboard.component'
+import { DashboardService } from './services/dashboard.service'
 
 @NgModule({
   declarations: [DashboardComponent],
--- src/app/shared/services/crud-service.ts
+++ src/app/shared/services/crud-service.ts
@@ -1,34 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { take } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
+import { take } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
+interface DefaultType {
+  id?: string | number
+}
 
 export class CrudService<T> {
   constructor(protected http: HttpClient, private API_URL: string) {}
 
   findAll(): Observable<T[]> {
-    return this.http.get<any>(this.API_URL);
+    return this.http.get<any>(this.API_URL)
   }
-  find(id: number) {
-    return this.http.get<T>(`${this.API_URL}/${id}`).pipe(take(1));
+  find(id: number): Observable<T> {
+    return this.http.get<T>(`${this.API_URL}/${id}`).pipe(take(1))
   }
-  private create(record: T) {
-    return this.http.post(this.API_URL, record).pipe(take(1));
+  private create<T extends DefaultType>(record: T): Observable<T> {
+    return this.http.post<T>(this.API_URL, record).pipe(take(1))
   }
 
-  private update(record: T) {
+  private update<T extends DefaultType>(record: T): Observable<T> {
     return this.http
-      .put(`${this.API_URL}/${record['id']}`, record)
-      .pipe(take(1));
+      .put<T>(`${this.API_URL}/${record.id}`, record)
+      .pipe(take(1))
   }
 
-  save(record: T) {
-    if (record['id']) {
-      return this.update(record);
+  save<T extends DefaultType>(record: T): Observable<T> {
+    if (record.id) {
+      return this.update(record)
     }
-    return this.create(record);
+    return this.create(record)
   }
 
-  remove(id: number) {
-    return this.http.delete(`${this.API_URL}/${id}`).pipe(take(1));
+  remove(id: number): Observable<T> {
+    return this.http.delete<T>(`${this.API_URL}/${id}`).pipe(take(1))
   }
 }
